d48684ccd27cb0b64b92b1f093a4bd1462f3c47e
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-08-07 17:48:59 +0900

    Feat : 상품의 사이즈와 컬러는 ","를 기준으로 배열로 변경 후 업로드 적용 #1

--- src/slice/productsManagement/createProductSlice.js
+++ src/slice/productsManagement/createProductSlice.js
@@ -18,9 +18,13 @@ export const createProductSlice = createSlice({
   reducers: {
     setNewProduct: (state, action) => {
       const { key, value } = action.payload;
-      console.log(action.payload);
-      state.newProduct = { ...state.newProduct, [key]: value };
-      console.log(state.newProduct);
+      // console.log(action.payload);
+      if (key === "size" || key === "color") {
+        state.newProduct = { ...state.newProduct, [key]: value.split(",") };
+      } else {
+        state.newProduct = { ...state.newProduct, [key]: value };
+      }
+      // console.log(state.newProduct);
     },
     resetNewProduct: (state) => {
       state.newProduct = {
