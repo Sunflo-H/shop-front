dc0763af025fa03eb5c2cca1daa0ed95d763baa3
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-08-22 07:59:42 +0900

    Feat : 상품리스트 - 체크박스 디자인 #6

--- src/components/management/main/ProductList/ProductListHeader.jsx
+++ src/components/management/main/ProductList/ProductListHeader.jsx
@@ -3,8 +3,33 @@ import React from "react";
 export default function ProductListHeader() {
   return (
     <div className="flex py-2 border-b border-gray-300 font-bold bg-blue-100 ">
-      <div className="w-20 flex justify-center items-center">
-        <input type="checkbox" />
+      <div className="w-20 flex justify-center items-center ">
+        <label
+          class="relative flex items-center rounded-full cursor-pointer"
+          htmlFor="checkbox"
+        >
+          <input
+            type="checkbox"
+            class="peer relative h-4 w-4 cursor-pointer appearance-none rounded border-2 border-blue-400 bg-white checked:border-blue-400 checked:bg-blue-400 "
+            id="checkbox"
+          />
+          <span class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3.5 w-3.5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              stroke="currentColor"
+              stroke-width="1"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </span>
+        </label>
       </div>
       <div className="w-60 text-lg text-blue-900">Name</div>
       <div className="w-40 text-lg text-blue-900">Price</div>
