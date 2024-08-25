3adb8e9cc8e8ac0f7147c1ff38aafc6efcd74798
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-08-25 23:15:29 +0900

    Feat : 헤더 삭제 #6

--- src/pages/management/ManagementApp.jsx
+++ src/pages/management/ManagementApp.jsx
@@ -1,5 +1,4 @@
 import { Provider } from "react-redux";
-import Header from "../../layout/manage/Header";
 import Main from "../../layout/manage/Main";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import store from "../../store";
@@ -12,8 +11,6 @@ export default function ManagementApp() {
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <div className="manage-font flex flex-col min-h-screen bg-lightblue">
-          {/* <div className="flex flex-col min-h-screen bg-gray-200 "> */}
-          {/* <Header /> */}
           <div className="flex self-center grow max-w-screen-2xl w-screen mt-4 ">
             <Nav />
             <div className="ml-72">
