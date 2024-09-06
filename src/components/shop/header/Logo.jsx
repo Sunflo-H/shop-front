37677564b5624923176442c108f99abd83d40879
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-09-06 10:17:32 +0900

    update : 높이 조절  #18

--- src/components/shop/header/Logo.jsx
+++ src/components/shop/header/Logo.jsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <Link
       to="/"
-      className="flex items-center text-3xl md:mr-6 xl:mr-20 pt-2 pb-3 md:pb-2 cursor-pointer"
+      className="flex items-center text-3xl md:mr-6 xl:mr-20 pt-3 pb-3 md:pb-2 cursor-pointer"
     >
       <h1>Adonis</h1>
     </Link>
--- src/components/shop/header/Nav.jsx
+++ src/components/shop/header/Nav.jsx
@@ -14,7 +14,7 @@ export default function Nav() {
 
   return (
     <nav
-      className="hidden grow items-center gap-1 h-100vh 
+      className="hidden grow items-stretch gap-1
     md:flex lg:gap-5 xl:gap-6"
     >
       {navItemList.map((navItem, i) => (
