b5771cde0ec542e7f336d21b9aa772fbf2c31c61
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-09-06 10:11:50 +0900

    Feat : 검색바 포커스, 블러 이벤트#18

--- src/components/shop/header/SearchBar.jsx
+++ src/components/shop/header/SearchBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 
 export default function SearchBar() {
@@ -5,13 +6,21 @@ export default function SearchBar() {
     // searchModalOpen 기능이 들어가야함
     console.log(1);
   };
+
+  const [isFocus, setIsFocus] = useState(false);
+
   return (
     <div className="hidden md:block mx-2 h-full" onClick={handleSearchClick}>
-      <div className="flex items-center border focus:border-black mt-3.5 ">
+      <div
+        className={`flex items-center mt-3.5 border
+        ${isFocus && "border-black"}`}
+      >
         <input
           type="text"
           placeholder="Search Adonis"
           className="p-2 text-sm focus:outline-none"
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
         />
         <AiOutlineSearch className="text-2xl mr-2 cursor-pointer" />
       </div>
