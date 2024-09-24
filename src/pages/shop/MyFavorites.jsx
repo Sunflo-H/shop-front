7e0a23d2f8fbc1bb8ad8a9405aa3a8bb36b66b1d
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-09-25 01:17:01 +0900

    Feat: favorite 페이지 완성 #21

--- src/pages/shop/MyFavorites.jsx
+++ src/pages/shop/MyFavorites.jsx
@@ -3,33 +3,42 @@ import useFavorites from "../../hooks/useFavorites";
 import { useEffect } from "react";
 import EmptyProduct from "../../pages/error/EmptyProduct";
 import ProductCard from "../../components/shop/main/product/ProductCard";
+import { useQuery } from "@tanstack/react-query";
+import { getProductsByIdList } from "../../api/productApi";
 
 export default function MyFavorites() {
-  const {
-    favoriteQuery: { data: favoriteProduct },
-  } = useFavorites();
+  const { data: user } = useQuery({ queryKey: ["loginedUser"] });
+  const { favoriteList } = user ?? {};
 
+  const { data: favoriteProducts } = useQuery({
+    queryKey: ["favoriteProducts"],
+    queryFn: () => getProductsByIdList(favoriteList),
+    enabled: !!user,
+  });
+
+  console.log(favoriteProducts);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-    <div className="pt-20 px-4">
-      <div className="text-center py-2 px-4 border-b">
-        <span className="text-xl font-bold">My Favorites</span>
+    <section className="pt-32 pb-10 px-4">
+      <div className="text-center mb-6">
+        <span className="text-2xl font-bold">My Favorites</span>
       </div>
-      {!favoriteProduct || favoriteProduct?.length === 0 ? (
+
+      {!favoriteList || favoriteList?.length === 0 ? (
         <EmptyProduct />
       ) : (
         <div
-          className={`grid grid-cols-2 md:grid-cols-3 mt-8
-        max-w-screen-2xl m-auto px-10 gap-5`}
+          className={`grid grid-cols-2 mx-4 max-w-screen-xl gap-10
+        md:grid-cols-3 md:m-auto`}
         >
-          {favoriteProduct?.map((product, index) => (
+          {favoriteProducts?.map((product, index) => (
             <ProductCard product={product} key={index} />
           ))}
         </div>
       )}
-    </div>
+    </section>
   );
 }
