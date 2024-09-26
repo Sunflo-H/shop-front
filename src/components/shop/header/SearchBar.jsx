2d60e55d09d9263c784dd4ae385c3d10e21113bb
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-09-26 22:46:07 +0900

    Feat: 값 입력받기 #22

--- src/components/shop/header/SearchBar.jsx
+++ src/components/shop/header/SearchBar.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar() {
-  const handleSearchClick = () => {
-    // searchModalOpen 기능이 들어가야함
-    console.log(1);
+  const navigate = useNavigate();
+  const [isFocus, setIsFocus] = useState(false);
+  const [searchInput, setSearchInput] = useState(false);
+
+  console.log(searchInput);
+  const handleSearchInputChange = (e) => {
+    setSearchInput(e.target.value);
   };
 
-  const [isFocus, setIsFocus] = useState(false);
+  const handleSearchClick = () => {
+    navigate("/products/search/?쿼리");
+    // 위 페이지에서 상품 데이터 렌더링할 수 있는 세팅해야지;
+  };
 
   return (
     <div className="hidden md:block mx-2 h-full" onClick={handleSearchClick}>
@@ -21,6 +29,7 @@ export default function SearchBar() {
           className="p-2 text-sm focus:outline-none"
           onFocus={() => setIsFocus(true)}
           onBlur={() => setIsFocus(false)}
+          onChange={handleSearchInputChange}
         />
         <AiOutlineSearch className="text-2xl mr-2 cursor-pointer" />
       </div>
