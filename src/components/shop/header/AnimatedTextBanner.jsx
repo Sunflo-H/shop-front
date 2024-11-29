ebf5a3a021ee9c94dfb847f6cdfc6e6a535e0604
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-11-29 23:53:09 +0900

    Update: marquee 2xl 반응형  #18 단순히 텍스트를 늘려서 만드는 방식에서 일정구간 무한 반복하는 방식으로 적용

--- src/components/shop/header/AnimatedTextBanner.jsx
+++ src/components/shop/header/AnimatedTextBanner.jsx
@@ -1,80 +1,38 @@
 import React from "react";
 
+const BannerScreen2xl = ({ text1, text2 }) => {
+  return (
+    <div className="flex items-center h-full   animate-marquee border">
+      <div className="flex  text-center ">
+        <div className="w-60">{text1}</div>
+        <div className="w-60">{text2}</div>
+      </div>
+      <div className="flex text-center ">
+        <div className="w-60">{text1}</div>
+        <div className="w-60">{text2}</div>
+      </div>
+      <div className="flex text-center ">
+        <div className="w-60">{text1}</div>
+        <div className="w-60">{text2}</div>
+      </div>
+      <div className="flex text-center ">
+        <div className="w-60">{text1}</div>
+        <div className="w-60">{text2}</div>
+      </div>
+      <div className="flex text-center ">
+        <div className="w-60">{text1}</div>
+        <div className="w-60">{text2}</div>
+      </div>
+    </div>
+  );
+};
+
 export default function AnimatedTextBanner({ text1, text2 }) {
   return (
-    <div className="flex items-center h-7 bg-black text-white text-[12px] font-bold overflow-hidden whitespace-nowrap">
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      {/* <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div> */}
-      {/* <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div> */}
-      {/* <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div> */}
-      {/* <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div>
-      <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div> */}
-      {/* <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div> */}
-      {/* <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div> */}
-      {/* <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div> */}
-      {/* <div className="flex text-center animate-marquee ">
-        <div className="w-60">{text1}</div>
-        <div className="w-60">{text2}</div>
-      </div> */}
+    // banner container
+    <div className="flex bg-black h-7 border font-bold text-[12px] text-white">
+      <BannerScreen2xl text1={text1} text2={text2} />
+      <BannerScreen2xl text1={text1} text2={text2} />
     </div>
   );
 }
