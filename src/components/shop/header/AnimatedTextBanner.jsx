50837e6201062edb723c8e7af1d148a9e49d8537
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2025-01-26 23:53:22 +0900

    Update: 배너 수정정 #18

--- src/components/shop/header/AnimatedTextBanner.jsx
+++ src/components/shop/header/AnimatedTextBanner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BannerScreen2xl = ({ text1, text2 }) => {
   return (
-    <div className="flex items-center h-full animate-marquee border">
+    <div className="flex items-center h-full animate-marquee">
       <div className="flex text-center ">
         <div className="w-60">{text1}</div>
         <div className="w-60">{text2}</div>
