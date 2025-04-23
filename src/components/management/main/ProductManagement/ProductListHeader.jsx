78b3dcafef3736eb0dbf4cae2fae78734deae32f
Sunflo-H <sunflo0524@gmail.com>
2025-04-24 00:46:05 +0900

    AllCheckBox 컴포넌트로 함수 이동

--- src/components/management/main/ProductManagement/ProductListHeader.jsx
+++ src/components/management/main/ProductManagement/ProductListHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setAllCheckBox,
@@ -13,31 +13,34 @@ const headerItem = [
   { value: "Price", width: "w-40" },
   { value: "Category", width: "w-40" },
   { value: "Status", width: "w-40" },
-  { value: "Creation Date", width: "w-40" },
+  { value: "Quantity", width: "w-40" },
 ];
 
 export default function ProductListHeader({ products }) {
-  const dispatch = useDispatch();
-  const idList = products.map((item) => item._id);
-  const { checkboxList, allCheckBox } = useSelector(
-    (state) => state.productManagement
-  );
+  // 기능을 AllCheckBox 컴포넌트로 이동시킴
+  // const dispatch = useDispatch();
+  // const idList = products.map((item) => item._id);
+  // const { checkboxList, allCheckBox } = useSelector(
+  //   (state) => state.productManagement
+  // );
 
-  const handleSelectAll = (e) => {
-    if (e.target.checked) {
-      dispatch(setAllCheckBox(true));
-      dispatch(setCheckboxList(checkboxList.map(() => true)));
-      dispatch(setIdList(idList));
-    } else {
-      dispatch(setAllCheckBox(false));
-      dispatch(setCheckboxList(checkboxList.map(() => false)));
-      dispatch(setIdList([]));
-    }
-  };
+  // // 전체선택버튼을 눌렀을 때
+  // const handleSelectAll = (e) => {
+  //   if (e.target.checked) {
+  //     dispatch(setAllCheckBox(true));
+  //     dispatch(setCheckboxList(checkboxList.map(() => true)));
+  //     dispatch(setIdList(idList));
+  //   } else {
+  //     dispatch(setAllCheckBox(false));
+  //     dispatch(setCheckboxList(checkboxList.map(() => false)));
+  //     dispatch(setIdList([]));
+  //   }
+  // };
 
   return (
     <div className="flex py-2 border-b border-gray-300 font-bold bg-blue-100 ">
-      <AllCheckBox onChange={handleSelectAll} allCheckBox={allCheckBox} />
+      {/* <AllCheckBox onChange={handleSelectAll} allCheckBox={allCheckBox} /> */}
+      <AllCheckBox products={products} />
       {headerItem.map((item, index) => (
         <DataListHeaderItem item={item} key={index} />
       ))}
--- src/components/management/main/ui/AllCheckBox.jsx
+++ src/components/management/main/ui/AllCheckBox.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setAllCheckBox,
+  setCheckboxList,
+  setIdList,
+} from "../../../../slice/management/productManagementSlice";
 
-export default function AllCheckBox({ onChange, allCheckBox }) {
+// export default function AllCheckBox({ onChange, allCheckBox }) {
+export default function AllCheckBox({ products }) {
+  const dispatch = useDispatch();
+  const idList = products.map((item) => item._id);
+  const { checkboxList, allCheckBox } = useSelector(
+    (state) => state.productManagement
+  );
+
+  // 전체선택버튼을 눌렀을 때
+  const handleSelectAll = (e) => {
+    if (e.target.checked) {
+      dispatch(setAllCheckBox(true));
+      dispatch(setCheckboxList(checkboxList.map(() => true)));
+      dispatch(setIdList(idList));
+    } else {
+      dispatch(setAllCheckBox(false));
+      dispatch(setCheckboxList(checkboxList.map(() => false)));
+      dispatch(setIdList([]));
+    }
+  };
   return (
     <div className="w-20 flex justify-center items-center">
       <label
@@ -12,7 +37,7 @@ export default function AllCheckBox({ onChange, allCheckBox }) {
           className={`peer relative h-4 w-4 cursor-pointer appearance-none rounded border-2 border-blue-400 bg-white checked:border-blue-400 checked:bg-blue-400 `}
           id="checkbox"
           checked={allCheckBox}
-          onChange={onChange}
+          onChange={handleSelectAll}
         />
         <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
           <svg
