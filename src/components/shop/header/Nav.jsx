c9407a51433e9013fcb7086087d905bf60e1912c
Sunflo-H <sunflo0524@gmail.com>
2025-04-27 00:27:06 +0900

    Nav 불필요한 코드 삭제 #4

--- src/components/shop/header/Nav.jsx
+++ src/components/shop/header/Nav.jsx
@@ -5,25 +5,10 @@ import { useParams } from "react-router-dom";
 const navItemList = ["Home", "Man", "Woman", "Accessory", "Shoes", "Recommend"];
 
 export default function Nav() {
-  const { category } = useParams();
-  const [activeNav, setActiveNav] = useState(null);
-
-  useEffect(() => {
-    setActiveNav(category);
-  }, [category]);
-
   return (
-    <nav
-      className="hidden grow items-stretch gap-1
-    md:flex lg:gap-5 xl:gap-6"
-    >
-      {navItemList.map((navItem, i) => (
-        <NavItem
-          navItem={navItem}
-          activeNav={activeNav}
-          setActiveNav={setActiveNav}
-          key={i}
-        />
+    <nav className="hidden grow items-stretch gap-1 md:flex lg:gap-5 xl:gap-6">
+      {navItemList.map((navItem) => (
+        <NavItem key={navItem} navItem={navItem} />
       ))}
     </nav>
   );
